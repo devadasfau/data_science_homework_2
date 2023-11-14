b99ac76d99525490f8da478e26829f54994be193
devadasfau <150597896+devadasfau@users.noreply.github.com>
2023-11-14 17:36:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='math_quiz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here, if applicable
+        # For example, if you use 'random' module in your program, you don't need to add it here.
+],
+)
